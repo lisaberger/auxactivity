1f8ad688c93b8262759e4b5760719e29368cb008
bergerli <Lissa972210!>
2021-11-30 12:12:04 +0100

    integrate category as one-to-many field in activity model

--- auxactivity/activities/admin.py
+++ auxactivity/activities/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 
 # Register your models here.
 from django.contrib import admin
-from .models import Activity
+from .models import Activity, Category
 
 admin.site.register(Activity)
+admin.site.register(Category)
--- auxactivity/activities/models.py
+++ auxactivity/activities/models.py
@@ -3,12 +3,11 @@ from django.db import models
 # Create your models here.
 
 
-'''class Category(models.Model):
+class Category(models.Model):
     name = models.CharField(max_length=200)
-'''
 
 
 class Activity(models.Model):
     name = models.CharField(max_length=200)
     description = models.TextField(null=True)
-    # categories = models.ManyToManyField(Category)
+    categories = models.ManyToManyField(Category)
