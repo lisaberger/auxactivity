3d45e73d2477ca268e5d6606d7b700269f7de632
Teresa Hauser <teresa.hauser@hs-augsburg.de>
2021-12-04 11:20:10 +0100

    Benutzer angelegt

--- auxactivity/activities/models.py
+++ auxactivity/activities/models.py
@@ -6,8 +6,11 @@ from django.db import models
 class Category(models.Model):
     name = models.CharField(max_length=200)
 
+# Klasse Aktivitäten
 
 class Activity(models.Model):
     name = models.CharField(max_length=200)
     description = models.TextField(null=True)
     categories = models.ManyToManyField(Category)
+
+
